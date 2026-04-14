c4127e910e42f097b63d2d57bf17d77fcb658341
andy <andyt.cyb@gmail.com>
2026-04-14 12:43:01 -0400

    (refactor) Refactored formatName conditionals with ternary

--- src/main/java/com.pluralsight/FullNameApplication.java
+++ src/main/java/com.pluralsight/FullNameApplication.java
@@ -11,7 +11,8 @@ public class FullNameApplication {
         String middleName = askString("Please enter your middle name: ");
         String lastName = askString("Please enter your last name: ");
         String suffix = askString("Please enter your suffix: ");
-        String fullName =  formatName(firstName, middleName, lastName,suffix);
+        String fullName =  formatedName(firstName, middleName, lastName, suffix);
+
         System.out.printf("Full Name: %s",fullName);
     }
 
@@ -20,16 +21,20 @@ public class FullNameApplication {
         return scanner.nextLine().trim();
     }
 
-    public static String formatName(String firstName, String middleName, String lastName, String suffix){
-        if (middleName.equals("") && suffix.equals("")) {
-            return firstName + " " + lastName;
-        } else if (!middleName.equals("") && suffix.equals("")){
-            return firstName + " " + middleName + ". " + lastName;
-        } else if(middleName.equals("") && !suffix.equals("")){
-            return firstName + " " + lastName + ", " + suffix;
-        }
-        return firstName + " " + middleName + ". " + lastName + ", " + suffix;
+    public static String formatedName(String firstName, String middleName, String lastName, String suffix){
+
+        return firstName + " " + (!middleName.isEmpty() ? " " + middleName + ". " : "") + lastName + (!suffix.isEmpty() ? ", " + suffix : "");
     }
+//        if (middleName.equals("") && suffix.equals("")) {
+//            return firstName + " " + lastName;
+//        } else if (!middleName.equals("") && suffix.equals("")){
+//            return firstName + " " + middleName + ". " + lastName;
+//        } else if(middleName.equals("") && !suffix.equals("")){
+//            return firstName + " " + lastName + ", " + suffix;
+//        }
+//        return firstName + " " + middleName + ". " + lastName + ", " + suffix;
+//    }
+
 
 
 }
